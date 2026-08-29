ee52c6dd0dc64535816a1faf15834cf9d6a9a062
kinyosampo1-030 <kinyosampo1@gmail.com>
2026-08-29 12:57:28 +0800

    Fix formatting issues in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1539,6 +1539,7 @@ const PackOpenerModal = ({ allCards, onClose, lang, user, userProfile, handleCla
             </label>
         </div>
 );
+};
         {/* 🌟 抽卡動畫展示區 */}
         <div className="flex-1 flex flex-col items-center justify-center min-h-[400px] relative">
           
